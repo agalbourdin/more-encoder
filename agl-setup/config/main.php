2c0cb67b75df2ff6aef69d6790cca03dec8d2279
Alexis Galbourdin <i@alexisg.net>
2013-05-19 17:40:58 +0200

    PHP install and config files.

---
+++ agl-setup/config/main.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+
+	/**
+	 * Key used to encode and decode strings.
+	 * Use a unique random string.
+	 */
+	'key' => ''
+
+);
---
+++ agl-setup/install.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+
+	'config/main.php' => array(
+		'dir'  => 'app/etc/config/more/encoder/',
+		'file' => 'main.php'
+	)
+
+);
