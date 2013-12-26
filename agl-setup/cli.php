39b807175bcc1c41fc14fc9923eba881d173ddb1
Alexis Galbourdin <i@alexisg.net>
2013-12-26 15:19:16 +0100

    Updated setup config and Composer dependencies.

--- agl-setup/cli.php
+++ agl-setup/cli.php
@@ -6,12 +6,12 @@ return array(
     // Copy configuration file.
     'file:copy' => array(
         $currentDir . 'app/etc/config/more/encoder/main.php',
-        APP_PATH    . 'app/etc/config/more/encoder/main.php'
+        $appPath    . 'app/etc/config/more/encoder/main.php'
     ),
 
     // Generate random key.
     'file:replace' => array(
-        APP_PATH . 'app/etc/config/more/encoder/main.php',
+        $appPath . 'app/etc/config/more/encoder/main.php',
         '"\'key\' => \'\'"',
         '"\'key\' => \'' . hash('MD5', openssl_random_pseudo_bytes(32)) . '\'"',
     )
